3491aa394a51aa8ef8c2ebdf983b2d02f094e0ac
carlospenaforte <carlosedu_1132@hotmail.com>
2025-04-19 18:31:12 -0300

    Create Models, Data folders. Connect to the server using defaultconnection. Maked migrations. update database

---
+++ VideoGameAPI/Data/VideoGameAPIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGameAPI.Models;
+
+namespace VideoGameAPI.Data
+{
+    public class VideoGameAPIDbContext : DbContext
+    {
+        public VideoGameAPIDbContext(DbContextOptions<VideoGameAPIDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<ProdutoModel> Produtos { get; set; }
+    }
+}
